869755a41a6dae3f08655c0c7757b00c489e2e76
kevin <kevinjdinicola@gmail.com>
2023-09-29 10:17:03 -0700

    gam

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,74 @@
-fn main() {
-    println!("Hello, world!");
+use bevy::core_pipeline::bloom::BloomSettings;
+use bevy::core_pipeline::tonemapping::Tonemapping;
+use bevy::prelude::*;
+
+
+#[derive(Component)]
+struct AnimateTranslation;
+
+fn main(){
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .add_systems(Update, animate_translation)
+        .add_systems(Update, update_bloom_settings)
+        .run();
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    // 2d camera
+    commands.spawn((
+        Camera2dBundle {
+            camera: Camera {
+                hdr: true, // 1. HDR is required for bloom
+                ..default()
+            },
+            tonemapping: Tonemapping::TonyMcMapface, // 2. Using a tonemapper that desaturates to white is recommended
+            ..default()
+        },
+        BloomSettings::default(), // 3. Enable bloom for the camera
+    ));
+
+    let font = asset_server.load("fonts/FiraSans-Bold.ttf");
+    let text_style = TextStyle {
+        font: font.clone(),
+        font_size: 60.0,
+        color: Color::WHITE,
+    };
+    let text_alignment = TextAlignment::Center;
+
+    commands.spawn((
+        Text2dBundle {
+            text: Text::from_section("MakGam!", text_style.clone())
+                .with_alignment(text_alignment),
+            ..default()
+        },
+        AnimateTranslation,
+    ));
+}
+
+fn animate_translation(
+    time: Res<Time>,
+    mut query: Query<&mut Transform, (With<Text>, With<AnimateTranslation>)>,
+) {
+    for mut transform in &mut query {
+        transform.translation.x = 100.0 * time.elapsed_seconds().sin();
+        transform.translation.y = 100.0 * time.elapsed_seconds().cos();
+    }
+}
+
+fn update_bloom_settings(
+    mut camera: Query<(Entity, Option<&mut BloomSettings>), With<Camera>>,
+    mut text: Query<&mut Text>,
+    mut commands: Commands,
+    keycode: Res<Input<KeyCode>>,
+    time: Res<Time>,
+) {
+    let bloom_settings = camera.single_mut();
+    match bloom_settings {
+        (_entity, Some(mut bloom_settings)) => {
+            bloom_settings.intensity = 1.0 * time.elapsed_seconds().sin()
+        }
+        _ => {}
+    }
 }
